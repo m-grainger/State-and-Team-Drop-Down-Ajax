8e8587da93e0529274a6267c8352886657c60f3d
m-grainger <grainger.books@gmail.com>
2017-07-30 15:11:06 -0700

    Add project files.

---
+++ StatesSports/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using StatesSports.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StatesSports.Controllers
+{
+    public class HomeController : Controller
+    {
+        SportsEntities db = new SportsEntities();
+        public ActionResult Index()
+
+        {
+            /** Creates list for the "Select a State" drop-down **/
+
+            List<State> StateList = db.States.ToList();
+            ViewBag.StateList = new SelectList(StateList, "StateId", "StateName");
+            return View();
+        }
+
+        /**
+            Function to be used in the Ajax request.
+            Passes StateId, runs a LINQ query on it, 
+            appends the result to TeamList,
+            and returns the data as Json.
+        **/
+        
+        public JsonResult GetTeamList(int StateId)
+        { 
+            db.Configuration.ProxyCreationEnabled = false;
+            List<Team> TeamList = db.Teams.Where(x => x.StateId == StateId).ToList();
+            return Json(TeamList, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
---
+++ StatesSports/Models/StateTeamViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StatesSports.Models
+{
+    public class StateTeamViewModel
+    {
+        public int StateId { get; set; }
+        public int TeamId { get; set; }
+    }
+}
